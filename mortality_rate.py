0f1354538328e2eb6517ee25f03a545852e11b6d
Oscar Ramirez <oramirez0045@g.ucla.edu>
2025-05-18 17:25:21 -0700

    Update mortality_rate.py

--- mortality_rate.py
+++ mortality_rate.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 f_ns = pd.read_csv('Female_Non-Smoker_2017.csv')
 f_ns.drop(f_ns.columns[2:], axis = 1, inplace = True)
+
+plt.plot(f_ns['Row\Column'], f_ns['1'], color = 'Green')
+plt.show()
