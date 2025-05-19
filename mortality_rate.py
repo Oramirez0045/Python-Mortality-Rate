752875c1d7b3f51cbd4ef24619d422f27c94b879
Oscar Ramirez <oramirez0045@g.ucla.edu>
2025-05-18 17:20:07 -0700

    Update mortality_rate.py

--- mortality_rate.py
+++ mortality_rate.py
@@ -1,3 +1,5 @@
-import matplotlib.pyplot
-import pandas
-a = pandas.read_csv(CSV-Files/Female_Non-Smoker_2017.csv)
+import matplotlib.pyplot as plt
+import pandas as pd
+
+f_ns = pd.read_csv('Female_Non-Smoker_2017.csv')
+f_ns.drop(f_ns.columns[2:], axis = 1, inplace = True)
