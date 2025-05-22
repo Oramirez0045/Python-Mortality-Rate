5c6e3b61695da9cc435a3683ed681d492a8554d6
ptan517 <ptan7@g.ucla.edu>
2025-05-21 23:59:37 -0700

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -1,38 +1,44 @@
 #https://www.geeksforgeeks.org/how-to-embed-matplotlib-charts-in-tkinter-gui/
+#https://matplotlib.org/stable/api/figure_api.html
+#https://matplotlib.org/stable/users/explain/axes/axes_intro.html
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 
 def plot():
-
-    fig = Figure(figsize = (5, 5), dpi = 100)
-
-    y = [i for i in range(500)]
-
-    plot1 = fig.add_subplot(111)
-
-    plot1.plot(y)
+    fig, ax = plt.subplots(figsize=(6, 6))
+    x = [3,2,95,24,235,0]
+    y = [1,4,3,2,3,5]
+    ax.plot(x, y)
 
     canvas = FigureCanvasTkAgg(fig, master = root)  
     canvas.draw()
-
-    canvas.get_tk_widget().grid(column=2, row=5, sticky=(W, E))
-
-    toolbar = NavigationToolbar2Tk(canvas, root, pack_toolbar = False)
-    toolbar.update()
-
-    canvas.get_tk_widget().grid(column=2, row=5, sticky=(W, E))
-
-###############################################################################################################
+    canvas.get_tk_widget().grid()
 
 #https://tkdocs.com/tutorial/firstexample.html
+#https://www.geeksforgeeks.org/setting-the-position-of-tkinter-labels/
+import tkinter as tk
 from tkinter import *
-from tkinter import ttk
 
-def calculate(*args):
-    try:
-        age_value = float(age.get())
-        sex_value = sex.get()
-        smoker_value = smoker.get()
+root = tk.Tk()
+
+#Labels to let the user know how to use the app
+age_label = tk.Label(root, text="Age")
+age_label.grid(row = 0, column = 0)
+
+sex_label = tk.Label(root, text="Sex (M/F)")
+sex_label.grid(row = 1, column = 0)
+
+smoker_label = tk.Label(root, text="Smoker? (Y/N)")
+smoker_label.grid(row = 2, column = 0)
+
+#This function will be run when the user clicks the button
+#It decides which graph to display based on the user's inputs
+def display_mortality(*args):
+    age_value = int(age.get())
+    sex_value = sex.get()
+    smoker_value = smoker.get()
+    if age_value > 0:
         if sex_value == "M":
             if smoker_value == "Y":
                 plot()
@@ -43,40 +49,22 @@ def calculate(*args):
                 plot()
             elif smoker_value == "N":
                 plot()
-    except ValueError:
-        pass
 
-root = Tk()
-root.title("Mortality Based on Age, Sex, and Smoking Status")
+#User input variables
+age = tk.StringVar()
+age_entry = tk.Entry(root, textvariable=age)
+age_entry.grid(row = 0, column = 5)
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+sex = tk.StringVar()
+sex_entry = tk.Entry(root, textvariable=sex)
+sex_entry.grid(row = 1, column = 5)
 
-age = StringVar()
-age_entry = ttk.Entry(mainframe, width=7, textvariable=age)
-age_entry.grid(column=2, row=1, sticky=(W, E))
+smoker = tk.StringVar()
+smoker_entry = tk.Entry(root, textvariable=smoker)
+smoker_entry.grid(row = 2, column = 5)
 
-sex = StringVar()
-sex_entry = ttk.Entry(mainframe, width=7, textvariable=sex)
-sex_entry.grid(column=2, row=2, sticky=(W, E))
-
-smoker = StringVar()
-smoker_entry = ttk.Entry(mainframe, width=7, textvariable=smoker)
-smoker_entry.grid(column=2, row=3, sticky=(W, E))
-
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=4, sticky=W)
-
-ttk.Label(mainframe, text="Age").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="Sex (M/F)").grid(column=3, row=2, sticky=W)
-ttk.Label(mainframe, text="Smoker? (Y/N)").grid(column=3, row=3, sticky=W)
-ttk.Label(mainframe, text="You projected mortality is").grid(column=1, row=5, sticky=E)
-
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
-
-age_entry.focus()
-root.bind("<Return>", calculate)
+#Button
+button = tk.Button(root, text="Display my mortality", command=display_mortality)
+button.grid(row = 5, column = 0)
 
 root.mainloop()
