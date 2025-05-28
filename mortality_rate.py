13d6832310c21affc0f09f841a4b98d307904df6
Oscar Ramirez <oramirez0045@g.ucla.edu>
2025-05-28 10:22:14 -0700

    Update mortality_rate.py

--- mortality_rate.py
+++ mortality_rate.py
@@ -2,64 +2,60 @@ import pandas as pd
 import plotly.graph_objects as go
 import os
 
-#function will look through csv file to find information on how to name a variable 
+#function looks through csv files, collecting information on how to name the files
 def naming(df):
-    gender = df.astype(str).apply(lambda col: col.str.contains('Female', case=False, na=False)).any().any()
-    if gender == True:
+    gender = df.astype(str).apply(lambda col: col.str.contains('Female')).any().any()
+    if gender:
         gender = 'Female'
     else:
-        gender = df.astype(str).apply(lambda col: col.str.contains('Gender-Blended', case=False, na=False)).any().any()
-        if gender == True:
-            gender = 'Mix'
-        if gender != 'Mix':
-            gender = 'Male'
-            
-    smokingStatus = df.astype(str).apply(lambda col: col.str.contains('Nonsmoker', case=False, na=False)).any().any()
-    if smokingStatus:
-        smokingStatus = 'Non-Smoker'
-    else:
-        smokingStatus = 'Smoker'
+        genderBlend = df.astype(str).apply(lambda col: col.str.contains('Gender-Blended')).any().any()
+        gender = 'Mix' if genderBlend else 'Male'
+
+    smokingStatus = df.astype(str).apply(lambda col: col.str.contains('Nonsmoker')).any().any()
+    smokingStatus = 'Non-Smoker' if smokingStatus else 'Smoker'
+    
     return gender + ' ' + smokingStatus
 
-folder = 'unclean_csv' #make sure to change this into the folder name that your csv files are in
+folder = 'csv'  #folder with uncleaned csv files (make sure to name this to the folder that contains the csv files)
 fileList = os.listdir(folder)
 
 figure = go.Figure()
 
 for file in fileList:
-    if file.endswith('.csv'):
-        #code to navigate towards csv folder
+    if file.endswith('.csv'): #only takes in csv files, just in case the folder isnt completely clean
         path = os.path.join(folder, file)
-        data = pd.read_csv(path, encoding = 'windows-1252')
-        
-        #uses function to find information within the file to create a name
-        name = naming(data)
-        
-        #data cleaning
-        data = data.iloc[111:, :2].reset_index(drop = True)  # Keep only the first two columns
-        data.columns = ['Age', 'Mortality Rate']
-        
-        #columns are originally objects and confuses plotly
-        data['Age'] = pd.to_numeric(data['Age'], errors='coerce')
-        data['Mortality Rate'] = pd.to_numeric(data['Mortality Rate'], errors='coerce')
+        data = pd.read_csv(path, encoding='windows-1252') #if encoding error, change encoding but i found that this one works
 
+        #uses function to name file
+        dfName = naming(data)
+
+        #cleaning data (only gets the second table)
+        data = data.iloc[111:, :2].reset_index(drop=True)
+        data.columns = ['Age', 'Mortality Rate']
+
+        #conver columns into numeric data (plotly seems to get confused if this isnt true)
+        data['Age'] = pd.to_numeric(data['Age'])
+        data['Mortality Rate'] = pd.to_numeric(data['Mortality Rate'])
+
+        #adds data to graph
         figure.add_trace(go.Scatter(
             x = data['Age'],
             y = data['Mortality Rate'],
             mode = 'markers',
             marker = dict(color='darkblue'),
-            name = rename(file),
-            showlegend = False
+            name = dfName,
+            showlegend = True
         ))
 
+        #saves info, this could be helpful for user interface so its able to interact and change
+        data.to_csv(f'clean_data/{name}.csv', index=False)
+
+# Finalize and show plot
 figure.update_layout(
     title = "Mortality Rate",
     xaxis_title = "Age",
     yaxis_title = "Mortality Rate",
     hovermode = "closest"
 )
-figure.add_vline(
-    x = 95,
-    line = dict(color="red", width=2),
-)
+figure.add_vline(x = 95, line = dict(color = "red", width = 2))
 figure.show()
